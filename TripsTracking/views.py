0d9ea61daf2db49aca6238ce001cc6c5c7ebd140
AngelikiMt <matta.d.angeliki@gmail.com>
2024-10-30 20:33:44 +0200

    create functions home and login

--- TripsTracking/views.py
+++ TripsTracking/views.py
@@ -1,2 +1,24 @@
-from flask import blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, redirect, url_for, request
+from flask_restful import Api
+from markupsafe import escape
 from jinja2 import TemplateNotFound
+from .static import login_form
+
+views = Blueprint("views", __name__, template_folder = 'templates')
+api = Api(views)
+
+@views.route("/<name>", methods=['GET'])
+def home(first_name, name=None):
+    if login:
+        return render_template('templates/', name=login.get(first_name))
+    return redirect(url_for('views.home'))
+
+@views.route("/TripLoginPage", methods=["GET", "POST"])
+def login():
+    form = login_form
+    if request == 'POST':
+        if form.valid_on_submit():
+            first_name = form.name.data
+            last_name = form.name.data
+            email = form.email.data
+    return redirect(url_for('views.login'))
