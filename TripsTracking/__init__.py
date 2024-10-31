02f7c1bb5de2438a5b597535e9b8c04badd07054
AngelikiMt <matta.d.angeliki@gmail.com>
2024-10-31 12:48:30 +0200

    login, logout, upload_images

--- TripsTracking/__init__.py
+++ TripsTracking/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config(SECRET_KEY='dev')
 
     with app.app_context():
         from .views import views
--- TripsTracking/views.py
+++ TripsTracking/views.py
@@ -1,24 +1,35 @@
-from flask import Blueprint, render_template, abort, redirect, url_for, request
+from flask import Blueprint, render_template, abort, redirect, url_for, request, session
 from flask_restful import Api
 from markupsafe import escape
 from jinja2 import TemplateNotFound
 from .static import login_form
+from werkzeug.utils import secure_filename 
 
 views = Blueprint("views", __name__, template_folder = 'templates')
 api = Api(views)
 
 @views.route("/<name>", methods=['GET'])
-def home(first_name, name=None):
-    if login:
-        return render_template('templates/', name=login.get(first_name))
-    return redirect(url_for('views.home'))
+def home(name=None):
+    if 'username' in session:
+        return f'{request.form['name']} you are logged in!'
+    return render_template('templates/', person=name)
 
-@views.route("/TripLoginPage", methods=["GET", "POST"])
-def login():
+
+@views.route("/TripsLogin", methods=["GET", "POST"])
+def login(name=None, error=None):
     form = login_form
-    if request == 'POST':
-        if form.valid_on_submit():
-            first_name = form.name.data
-            last_name = form.name.data
-            email = form.email.data
-    return redirect(url_for('views.login'))
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+    return render_template('views.home', person=name, error=error)
+
+@views.route("/TriopsLogout", methods=['GET'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+@views.route("/UploadImagies", methods=['GET', 'POST'])
+def upload_imagies():
+    if request.method == 'POST':
+        file = request.files['the_file']
+        file.save(f'{secure_filename(file.filename)}')
